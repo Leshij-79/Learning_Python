22af48a9ab7a360b9653f67246b67f7755ef0732
Kuprin Aleksandr <freeman8901@yandex.ru>
2025-10-06 22:13:25 +0500

    Тестирование модуля masks

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,6 +1,26 @@
-# def test_get_mask_card_number(card_number: str) -> str:
-#     pass
-#
-#
-# def test_get_mask_account(account_number: str) -> str:
-#     pass
+import pytest
+
+from src.masks import get_mask_card_number, get_mask_account
+
+
+@pytest.mark.parametrize('test_card_number, expected', [
+    ('1234567890123456', '1234 56** **** 3456'),
+    ('6543210987654321', '6543 21** **** 4321'),
+    ('234567890123456', 'Не верный номер карты'),
+    ('1234fff890123456', 'Не верный номер карты'),
+    ('1234 67890123456', 'Не верный номер карты'),
+    ('', 'Номер карты не введен'),
+])
+def test_get_mask_card_number(test_card_number: str, expected: str):
+    assert get_mask_card_number(test_card_number) == expected
+
+@pytest.mark.parametrize('test_account_number, expected', [
+    ('12345678901234567890', '**7890'),
+    ('09876543210987654321', '**4321'),
+    ('2345678901234567890', 'Не верный номер счёта'),
+    ('1234fff8901234567890', 'Не верный номер счёта'),
+    ('1234 678901234567890', 'Не верный номер счёта'),
+    ('', 'Номер счёта не введен'),
+])
+def test_get_mask_account(test_account_number: str, expected: str):
+    assert get_mask_account(test_account_number) == expected
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -15,7 +15,7 @@ from tests.conftest import (fixture_test_filter_by_state, fixture_test_filter_by
         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
     ])
 ])
-def test_filter_by_state_first(fixture_test_filter_by_state: list, test_name_state: str, expected: list) -> list:
+def test_filter_by_state_first(fixture_test_filter_by_state: list, test_name_state: str, expected: list):
     assert filter_by_state(fixture_test_filter_by_state, test_name_state) == expected
 
 
@@ -23,7 +23,7 @@ def test_filter_by_state_first(fixture_test_filter_by_state: list, test_name_sta
     ('EXECUTED', 'Нет данных'),
     ('CANCELED', 'Нет данных')
 ])
-def test_filter_by_state_second(test_name_state: str, expected: list) -> list:
+def test_filter_by_state_second(test_name_state: str, expected: list):
     assert filter_by_state([], test_name_state) == expected
 
 
@@ -41,7 +41,7 @@ def test_filter_by_state_second(test_name_state: str, expected: list) -> list:
         {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}
     ])
 ])
-def test_sort_by_date_first(fixture_test_filter_by_date_first: list, test_reverse: bool, expected_date: list) -> list:
+def test_sort_by_date_first(fixture_test_filter_by_date_first: list, test_reverse: bool, expected_date: list):
     assert sort_by_date(fixture_test_filter_by_date_first, test_reverse) == expected_date
 
 
@@ -59,7 +59,7 @@ def test_sort_by_date_first(fixture_test_filter_by_date_first: list, test_revers
         {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}
     ])
 ])
-def test_sort_by_date_first_second(fixture_test_filter_by_date_second: list, test_reverse: bool, expected_date: list) -> list:
+def test_sort_by_date_first_second(fixture_test_filter_by_date_second: list, test_reverse: bool, expected_date: list):
     assert sort_by_date(fixture_test_filter_by_date_second, test_reverse) == expected_date
 
 
@@ -87,6 +87,6 @@ def test_sort_by_date_first_second(fixture_test_filter_by_date_second: list, tes
         {'id': 939719570, 'state': 'EXECUTED', 'date': '30-06-2018T02:08:58.425572'}
     ])
 ])
-def test_sort_by_date_first_third(list_by_filter: list, test_reverse: bool, expected_date: list) -> list:
+def test_sort_by_date_first_third(list_by_filter: list, test_reverse: bool, expected_date: list):
     assert sort_by_date(list_by_filter, test_reverse) == expected_date
 
