4fc0d72db94a9965ac51a65d208de7bfaf24cac9
Kuprin Aleksandr <freeman8901@yandex.ru>
2025-10-06 21:08:27 +0500

    Тестирование модуля processing

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,6 +1,36 @@
-def test_filter_by_state(bank_operaion: list, name_state: str = "EXECUTED") -> list:
-    pass
+import pytest
+
+from src.processing import filter_by_state, sort_by_date
+from tests.conftest import fixture_test_filter_by_state, fixture_test_filter_by_date
 
 
-def test_sort_by_date(bank_operation: list, reverce_direcion: bool = True) -> list:
-    pass
+@pytest.mark.parametrize("test_name_state, expected", [
+    ('EXECUTED', [
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+    ]),
+    ('CANCELED', [
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    ])
+])
+def test_filter_by_state(fixture_test_filter_by_state: list, test_name_state: str, expected: list) -> list:
+    assert filter_by_state(fixture_test_filter_by_state, test_name_state) == expected
+
+
+@pytest.mark.parametrize("test_reverse, expected_date", [
+    (True, [
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+    ]),
+    (False, [
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}
+    ])
+])
+def test_sort_by_date(fixture_test_filter_by_date: list, test_reverse: bool, expected_date: list) -> list:
+    assert sort_by_date(fixture_test_filter_by_date, test_reverse) == expected_date
